c8a3096ccf73d70410700bbb203e084784d6dc43
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2021-12-10 21:04:50 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import HomePage from './screens/HomePage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomePage />
     </View>
   );
 }
